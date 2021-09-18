fd76b04adf3452a274e54767979dd2781f509dd7
Fedaa <fedaaessakhalaf@gmail.com>
2021-09-18 21:30:54 +0300

    First commit

---
+++ songs/src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+
+const Nav = ({ setLibraryStatus, libraryStatus }) => {
+  const openLibraryHandler = () => {
+    setLibraryStatus(!libraryStatus);
+  };
+
+  return (
+    <nav>
+      <h1>Normadie</h1>
+      <button
+        className={libraryStatus ? "library-active" : ""}
+        //to make the library button functionable
+        onClick={openLibraryHandler}
+      >
+        Library
+        <FontAwesomeIcon icon={faMusic}></FontAwesomeIcon>
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ songs/src/data.js
@@ -0,0 +1,121 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+    {
+      name: "Cabaña",
+      cover: "https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg",
+        artist: "Juan Rios",
+      audio: "https://chillhop.com/?p=10349",
+      color: ["#FFBBCC", "#FFDDCC"],
+      id: uuidv4(),
+      active: false,
+
+    },
+    {
+      name: "What A Time",
+      cover: "https://images.genius.com/c31eb1147cc8b51ae36642b5e821dcdf.1000x1000x1.png",
+        artist: "Julia Michaels (feat. Niall Horan)",
+      audio: "https://chillhop.com/?p=10349",
+      color: ["#FFBBCC", "#FFDDCC"],
+      id: uuidv4(),
+      active: false,
+
+    },
+    {
+      name: "Woman",
+      cover: "https://i.ytimg.com/an/2V_uAAAH-_Q/7608203655284526850_mq.jpg?v=60e4a32f",
+        artist: "Doja Cat)",
+      audio: "https://chillhop.com/?p=10349",
+      color: ["#FFBBCC", "#FFDDCC"],
+      id: uuidv4(),
+      active: false,
+
+    },
+    {
+      name: "Skaldhakker",
+      cover: "https://angartwork.akamaized.net/?id=144027094&size=640",
+        artist: "Danheim & Heldom-Domadagr",
+      audio:"https://skaldhakker.mp3quack.lol/",
+      color: ["#FFBBCC", "#FFDDCC"],
+      id: uuidv4(),
+      active: false,
+
+    },
+    {
+      name: "Motive",
+      cover: "https://cdns-images.dzcdn.net/images/cover/82043641a4e2852b1794110b74a302e8/350x350.jpg",
+        artist: "Ariana Grande & Doja Cat",
+      audio:"https://skaldhakker.mp3quack.lol/",
+      color: ["#FFBBCC", "#FFDDCC"],
+      id: uuidv4(),
+      active: false,
+
+    },
+
+
+
+  ];
+}
+
+export default chillHop;
